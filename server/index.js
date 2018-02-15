a9a89104cabf61945b34efe87b75bc9238f5c264
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-02-15 16:46:54 -0600

    created a get and post request to server

--- server/index.js
+++ server/index.js
@@ -8,9 +8,32 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
 
 
+app.get('/name', function(req, res) {
+  database.seletAll((err, results) => {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.status(200).json(results);
+    }
+  })
+})
 
+app.post('/name', function(req, res) {
+  let letter = req.body.letter;
+  let name = req.body.name;
 
-
+  if(!name) {
+    res.sendStatus(400);
+  } else {
+    database.getName(letter, name, (err, results) => {
+      if(err) {
+        res.sendStatus(500);
+      } else {
+        res.status(200).json(results);
+      }
+    });
+  }
+});
 
 
 
